f890621e1a577618bcc16758f85fa97242e3c9dc
IMP1 <IMP1@mail.com>
2014-11-17 23:04:07 +0000

    added basic page outline

---
+++ controllers/bootable.py
@@ -0,0 +1,12 @@
+def search():
+    return dict()
+
+def create():
+    return dict()
+
+def edit():
+    return dict()
+
+def view():
+    return dict()
+
---
+++ controllers/user.py
@@ -0,0 +1,20 @@
+def dashboard():
+    return dict()
+
+def login():
+    return dict()
+
+def new():
+    form = SQLFORM.factory(db.user_address, db.user_credit_card, db.user)
+    form.custom.widget.username["_placeholder"] = "Username"
+    form.custom.widget.username["_class"] += " date-short"
+    
+    if form.process().accepted:
+        id = db.client.insert(**db.client._filter_fields(form.vars))
+        form.vars.client=id
+        id = db.address.insert(**db.address._filter_fields(form.vars))
+        response.flash='Thanks for filling the form'
+    return dict(form = form)
+
+def profile():
+    return dict()
