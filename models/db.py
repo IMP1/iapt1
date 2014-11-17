a867ddacd33f9a017c60346b326cbefc5f4a30a4
IMP1 <IMP1@mail.com>
2014-11-17 23:07:28 +0000

    created database structure

--- models/db.py
+++ models/db.py
@@ -1,12 +1,50 @@
 db = DAL('sqlite://store.db')
 
-db.define_table('products', 
-                Field('name'), 
-                Field('price'), 
-                Field('type'), 
-                Field('description'), 
-                Field('publisher')
-               )
-db.define_table('features', 
-                Field('product_id', db.products)
-               )
+db.define_table('bootable_category', 
+                    Field('name', 'string', requires=IS_NOT_EMPTY())
+                )
+db.define_table('bootable_status', 
+                    Field('name', 'string', requires=IS_NOT_EMPTY())
+                )
+db.define_table('bootable', 
+                    Field('title', 'string', requires=IS_NOT_EMPTY()), 
+                    Field('summary', 'string', requires=IS_LENGTH(120)),
+                    Field('category_id', db.bootable_category),
+                    Field('status_id', db.bootable_status),
+                    Field('fundingGoal', 'double', requires=IS_FLOAT_IN_RANGE(0, 1e100)),
+                    Field('image', 'upload'),
+                    Field('description', 'text'),
+                    Field('about', 'text'),
+                    # Available Pledges are referenced from `bootable_pledges`.
+                    # Pledges made are referenced from `bootable_pledges_made`.
+                )
+db.define_table('bootable_pledges', 
+                    Field('bootable_id', db.bootable),
+                    Field('cost', 'double', requires=IS_FLOAT_IN_RANGE(0, 1e100)),
+                    Field('reward', 'text'),
+                )
+db.define_table('user_address', 
+                    Field('street_address', 'string', requires=IS_NOT_EMPTY()),
+                    Field('city', 'string', requires=IS_NOT_EMPTY()),
+                    Field('country', 'string', requires=IS_NOT_EMPTY()),
+                    Field('postcode', 'string', requires=IS_LENGTH(7)),
+                )
+db.define_table('user_credit_card', 
+                    Field('card_number', 'string', requires=IS_LENGTH(12)),
+                    Field('identifying_code', 'string', requires=IS_LENGTH(3)),
+                    Field('expiry_date', 'date'),
+                    Field('billing_address_id', db.user_address),
+                )
+db.define_table('user', 
+                    Field('username', 'string', requires=[IS_NOT_EMPTY(), IS_NOT_IN_DB(db, 'user.username')]),
+                    Field('real_name', 'string', requires=IS_NOT_EMPTY()),
+                    Field('birthdate', 'date'),
+                    Field('address_id', db.user_address),
+                    Field('credit_card_id', db.user_credit_card),
+                    
+                )
+db.define_table('bootable_pledges_made', 
+                    Field('bootable_id', db.bootable),
+                    Field('pledge_id', db.bootable_pledges),
+                    Field('user_id', db.user),
+                )
