f79016946bd9fdb7a451976a367159233e4ec4d7
IMP1 <IMP1@mail.com>
2014-11-22 17:21:10 +0000

    no longer checks for non-existant checkbox

--- static/js/func.js
+++ static/js/func.js
@@ -1,5 +1,4 @@
 function toggle_billing_address(checkbox) {
-    alert("toggled");
     var inputs = checkbox.parentElement.getElementsByTagName("input");
     var i;
     for (i = 0; i < inputs.length; i ++) {
@@ -19,15 +18,9 @@ function calculateHidden(stage) {
         document.getElementById("postcode").value = document.getElementById("postcode1").value + 
                                                     document.getElementById("postcode2").value;
     } else if (stage == 1) {
-        if (document.getElementById("toggle_address_button").checked) {
-//            document.getElementById("street_address").value = "{{ =session.address_vars['street_address']" }};
-//            document.getElementById("city").value = "{{ =session.address_vars['city'] }}";
-//            document.getElementById("postcode").value = "{{ =session.address_vars['postcode'] }}";
-//            document.getElementById("country").value = "{{ =session.address_vars['country'] }}";
-        } else {
-            document.getElementById("postcode").value = document.getElementById("postcode1").value + 
-                                                        document.getElementById("postcode2").value;
-        }
+        document.getElementById("postcode").value = document.getElementById("postcode1").value + 
+                                                    document.getElementById("postcode2").value;
+        alert("calcluated 2nd postcode.");
     }
     return true;
 }
