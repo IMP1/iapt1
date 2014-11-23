ce4f3e99a92de0ee3fd8d877850dbfa294bb3ffd
IMP1 <IMP1@mail.com>
2014-11-23 00:38:26 +0000

    put tables in dependency order and added published table

--- models/db.py
+++ models/db.py
@@ -1,32 +1,12 @@
 # Create Database
 db = DAL('sqlite://store.db')
 
-# Bootable Categories
+# Database Tables
 db.define_table('bootable_category', 
                     Field('name', 'string', requires=IS_NOT_EMPTY())
                 )
 db.define_table('bootable_status', 
                     Field('name', 'string', requires=IS_NOT_EMPTY())
-                )
-db.define_table('bootable', 
-                    Field('title', 'string', requires=IS_NOT_EMPTY()), 
-                    Field('summary', 'string', requires=IS_LENGTH(120)),
-                    Field('category_id', db.bootable_category),
-                    Field('status_id', db.bootable_status),
-                    Field('fundingGoal', 'double', requires=IS_FLOAT_IN_RANGE(0, 1e100)),
-                    Field('image', 'upload'),
-                    Field('description', 'text'),
-                    Field('about', 'text'),
-                    Field('creation_date', 'date'),
-                    Field('publish_date', 'date'),
-                    # Available Pledges are referenced from `bootable_pledges`.
-                    # Pledges made are referenced from `bootable_pledges_made`.
-                )
-db.define_table('bootable_pledges', 
-                    Field('bootable_id', db.bootable),
-                    Field('title', 'string', requires=IS_NOT_EMPTY()),
-                    Field('cost', 'double', requires=IS_FLOAT_IN_RANGE(0, 1e100)),
-                    Field('reward', 'text'),
                 )
 db.define_table('user_address', 
                     Field('street_address', 'string', requires=IS_NOT_EMPTY()),
@@ -46,7 +26,30 @@ db.define_table('user',
                     Field('birthdate', 'date'),
                     Field('address_id', db.user_address),
                     Field('credit_card_id', db.user_credit_card),
-                    
+                )
+db.define_table('bootable', 
+                    Field('title', 'string', requires=IS_NOT_EMPTY()), 
+                    Field('category_id', db.bootable_category),
+                    Field('summary', 'string', requires=IS_LENGTH(120)),
+                    Field('description', 'text'),
+                    Field('status_id', db.bootable_status),
+                    Field('author', db.user),
+                    Field('fundingGoal', 'double', requires=IS_FLOAT_IN_RANGE(0, 1e100)),
+                    Field('image', 'upload'),
+                    Field('about', 'text'),
+                    Field('creation_date', 'date'),
+                    # Available Pledges are referenced from `bootable_pledges`.
+                    # Pledges made are referenced from `bootable_pledges_made`.
+                )
+db.define_table('bootable_published',
+                    Field('bootable_id', db.bootable),
+                    Field('publish_date', 'date'),
+                )
+db.define_table('bootable_pledges', 
+                    Field('bootable_id', db.bootable),
+                    Field('title', 'string', requires=IS_NOT_EMPTY()),
+                    Field('cost', 'double', requires=IS_FLOAT_IN_RANGE(0, 1e100)),
+                    Field('reward', 'text'),
                 )
 db.define_table('bootable_pledges_made', 
                     Field('pledge_id', db.bootable_pledges),
