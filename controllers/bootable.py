f90de2711adb4cda19edd5ba561b10c2cc56fe3d
IMP1 <IMP1@mail.com>
2014-11-23 00:14:21 +0000

    redirect when not logged in.

--- controllers/bootable.py
+++ controllers/bootable.py
@@ -1,10 +1,15 @@
 def search():
-    return dict()
+    search_vars = request.vars
+    return dict(search_vars = search_vars)
 
 def create():
+    if not session.logged_in_user:
+        redirect(URL('default', 'index.html'))
     return dict()
 
 def edit():
+    if not session.logged_in_user:
+        redirect(URL('default', 'index.html'))
     return dict()
 
 def view():
