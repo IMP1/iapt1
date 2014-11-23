8bacc1242b627b6d34ba2d6c78d16e8cee674e11
IMP1 <IMP1@mail.com>
2014-11-23 16:36:21 +0000

    fixes #17

--- controllers/user.py
+++ controllers/user.py
@@ -59,8 +59,18 @@ def new():
     if session.registration_stage == 0 and form1.validate(formname='basic-info'):
         # Capture the address in case the user will want the billing address to be the same.
         session.address_vars = db.user_address._filter_fields(form1.vars)
-        # Capture the address ID after adding it to the database.
-        address_id = db.user_address.insert(**db.user_address._filter_fields(form1.vars))
+        # Check if an identical address exists in the database.
+        id = None
+        for address in db(db.user_address.id > 0).select():
+            for key in address:
+                if key != "id" and address[key] == session.address_vars[key]:
+                    id = address.id
+                    break
+        # Capture the address ID after adding it to the database if necessary.
+        if id != None:
+            address_id = id
+        else:
+            address_id = db.user_address.insert(**db.user_address._filter_fields(form1.vars))
         # Captures the user variables, but do not add it to the database yet.
         session.user_vars = db.user._filter_fields(form1.vars)
         # Add the address ID to the user.
