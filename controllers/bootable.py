7941276f6a401f2ce42a5169bd373014aafb993b
IMP1 <IMP1@mail.com>
2014-11-28 18:33:32 +0000

    added deleting of bootables and pledges in edit mode

--- controllers/bootable.py
+++ controllers/bootable.py
@@ -1,8 +1,10 @@
+## BOOTABLE SEARCH PAGE
 def search():
     session.clear()
     search_vars = request.vars
     return dict(search_vars = search_vars)
 
+## BOOTABLE CREATION PAGE
 def create():
     if not session.logged_in_user:
         session.redirection = URL('bootable', 'create.html')
@@ -17,12 +19,12 @@ def create():
     form2 = SQLFORM.factory(db.bootable.description, db.bootable.author)
     form3 = SQLFORM.factory(db.bootable_pledges)
     
-    # If we're deleting we don't need the form to be filled out, but it needs to pass validation for a formkey to be created
+    # If we're deleting we don't need the form to be filled out, but it needs to pass validation for a formkey to be created.
     placeholders = {}
     # If a delete pledge button has been pressed:
-    if request.vars.deletion and request.vars.creation_stage == 2:
+    if request.vars.pledge_deletion and request.vars.creation_stage == 2:
         # Delete the pledge:
-        db(db.bootable_pledges.id == int(request.vars.deletion)).delete()
+        db(db.bootable_pledges.id == int(request.vars.pledge_deletion)).delete()
         # If we used placeholder field values to placate web2py's form validation (we don't need it to delete pledges):
         placeholders['title'] = request.vars['title']
         request.vars['title'] = "Placeholder Title"
@@ -31,7 +33,7 @@ def create():
     
     # If the third form has been submitted:
     if form3.validate(formname='pledges') and request.vars.creation_stage == 2:
-        if request.vars.deletion:
+        if request.vars.pledge_deletion:
             response.flash = "Pledge deleted."
         else:
             # If the button pressed was the finish button:
@@ -85,7 +87,7 @@ def create():
         request.vars.creation_stage = 1
         response.flash = "Not much more to do!"
     
-    # If we've used placeholders, put them back
+    # If we've used placeholders, put them back.
     for key in placeholders:
         request.vars[key] = placeholders[key]
     
@@ -93,20 +95,63 @@ def create():
     stage = request.vars.creation_stage
     return dict(form = forms[stage], stage = stage, formkey = forms[stage].formkey)
 
-
+## BOOTABLE EDIT PAGE
 def edit():
     if not session.logged_in_user:
         session.redirection = URL('bootable', 'edit.html')
         session.flash = SPAN('You are not currently signed in. Sign in or ', A('Register', _href=URL('user', 'new.html')), '!')
         redirect(URL('user', 'login.html'))
+    if db.bootable(request.vars.bootable_id).author != session.logged_in_user:
+        redirect(URL('bootable', 'view.html', vars={'bootable_id': request.vars.bootable_id}))
         
-    form = SQLFORM.factory(db.bootable, db.bootable_pledges)
-
-    if form.validate():
-        pass
+    form1 = SQLFORM.factory(db.bootable)
+    form2 = SQLFORM.factory(db.bootable_pledges)
     
-    return dict(form = form, formkey = form.formkey)
+    # If we're deleting we don't need the form to be filled out, but it needs to pass validation for a formkey to be created.
+    placeholders = {}
+    # If we're deleting the bootable:
+    if request.vars.bootable_deletion:
+        # Delete the bootable.
+        pass
+    # If we're deleting a pledge:
+    elif request.vars.pledge_deletion:
+        # Delete the pledge.
+        db(db.bootable_pledges.id == int(request.vars.pledge_deletion)).delete()
+        # If we used placeholder field values to placate web2py's form validation (we don't need it to delete pledges).
+        placeholders['title'] = request.vars['title']
+        request.vars['title'] = "Placeholder Title"
+        placeholders['cost'] = request.vars['cost']
+        request.vars['cost'] = 0
+    
+    # If we are adding a pledge:
+    if form2.validate(formname="pledge"):
+        if request.vars.pledge_deletion:
+            response.flash = "Pledge deleted!"
+        else:
+            response.flash = "Pledge added!"
+            # Add the id to the pledge.
+            form2.vars.bootable_id = request.vars.bootable_id
+            # Add the new pledge to the database.
+            db.bootable_pledges.insert(**db.bootable_pledges._filter_fields(form2.vars))
+    
+    # If we're updating the bootable:
+    if form1.validate(formname="bootable") and not request.vars.bootable_deletion:
+        # Update
+        response.flash = "Bootable updated!"
+        # If we're updating and publishing:
+        if request.vars.publish:
+            # Publish
+            record = db.bootable(request.vars.publish)
+            updates = {'status_id': 2}
+            record.update_record(**db.bootable._filter_fields(updates))
+        
+    # If we've used placeholders, put them back.
+    for key in placeholders:
+        request.vars[key] = placeholders[key]
+        
+    return dict(form1 = form1, form2 = form2, formkey1 = form1.formkey, formkey2 = form2.formkey)
 
+## BOOTABLE PAGE VIEW
 def view():
     return dict()
 
