721dd2c23bd2770156e4707b0b1489495ffec55a
IMP1 <IMP1@mail.com>
2014-12-01 00:03:23 +0000

    duplicate addresses are better than bugs where people have the same address (even when they don't)

--- controllers/user.py
+++ controllers/user.py
@@ -48,29 +48,12 @@ def new():
     
     # If the second form has been submitted:
     if form2.validate(formname='billing-info') and request.vars.registration_stage == 1:
-        # Check if it's the same address.
-        if session.address_vars == db.user_address._filter_fields(form2.vars):
-            # Use the previous address (held in session vars).
-            billing_address_id = session.user_vars["address_id"]
-        else:
-            # Otherwise, get the address values.
-            billing_address_id = db.user_address.insert(**db.user_address._filter_fields(form2.vars))
+        billing_address_id = db.user_address.insert(**db.user_address._filter_fields(form2.vars))
         # Captures the credit card variables.
         credit_card_vars = db.user_credit_card._filter_fields(form2.vars)
         # Adds the billing address ID to the credit card.
         credit_card_vars["billing_address_id"] = billing_address_id
-        # Check if an identical credit card exists in the databas.
-        id = None
-        for credit_card in db(db.user_credit_card.id > 0).select():
-            for key in credit_card:
-                if key != "id" and key in credit_card_vars and credit_card[key] == credit_card_vars[key]:
-                    id = credit_card.id
-                    break
-        # Add the credit card to the database, and get its ID.
-        if id != None:
-            credit_card_id = id
-        else:
-            credit_card_id = db.user_credit_card.insert(**credit_card_vars)
+        credit_card_id = db.user_credit_card.insert(**credit_card_vars)
         # Add the credit card to the user stored in the session.
         session.user_vars["credit_card_id"] = credit_card_id
         # Finally add the user to the database.
@@ -95,18 +78,7 @@ def new():
     if form1.validate(formname='basic-info') and request.vars.registration_stage == 0:
         # Capture the address in case the user will want the billing address to be the same.
         session.address_vars = db.user_address._filter_fields(form1.vars)
-        # Check if an identical address exists in the database.
-        id = None
-        for address in db(db.user_address.id > 0).select():
-            for key in address:
-                if key != "id" and key in session.address_vars and address[key] == session.address_vars[key]:
-                    id = address.id
-                    break
-        # Capture the address ID after adding it to the database if necessary.
-        if id != None:
-            address_id = id
-        else:
-            address_id = db.user_address.insert(**db.user_address._filter_fields(form1.vars))
+        address_id = db.user_address.insert(**db.user_address._filter_fields(form1.vars))
         # Captures the user variables, but do not add it to the database yet.
         session.user_vars = db.user._filter_fields(form1.vars)
         # Add the address ID to the user.
